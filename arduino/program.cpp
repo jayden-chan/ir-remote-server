0f415721acb742a06e5822f8ce0093e8fe2b3dcb
Jayden Chan <jaydencn7@gmail.com>
2021-01-04 21:15:04 -0700

    JC: Arduino OTA updates

--- arduino/program.cpp
+++ arduino/program.cpp
@@ -27,76 +27,16 @@ const char *password = STAPSK;
 const char *host = SERVER_HOST;
 const uint16_t port = SERVER_PORT;
 
-/* ==================== start of TUNEABLE PARAMETERS ====================
- * An IR detector/demodulator is connected to GPIO pin 14
- * e.g. D5 on a NodeMCU board.
- * Note: GPIO 16 won't work on the ESP8266 as it does not have interrupts.
- */
 const uint16_t kRecvPin = 14;
 const uint16_t kIrLed = 4; // ESP8266 GPIO send pin to use. Recommended: 4 (D2).
-
-/* The Serial connection baud rate.
- * i.e. Status message will be sent to the PC at this baud rate.
- * Try to avoid slow speeds like 9600, as you will miss messages and
- * cause other problems. 115200 (or faster) is recommended.
- * NOTE: Make sure you set your Serial Monitor to the same speed.
- */
 const uint32_t kBaudRate = 115200;
 
-/* As this program is a special purpose capture/decoder, let us use a larger
- * than normal buffer so we can handle Air Conditioner remote codes.
- */
 const uint16_t kCaptureBufferSize = 1024;
 const uint8_t kTimeout = 15;
 
-/* Alternatives:
- * const uint8_t kTimeout = 90;
- * Suits messages with big gaps like XMP-1 & some aircon units, but can
- * accidentally swallow repeated messages in the rawData[] output.
- *
- * const uint8_t kTimeout = kMaxTimeoutMs;
- * This will set it to our currently allowed maximum.
- * Values this high are problematic because it is roughly the typical boundary
- * where most messages repeat.
- * e.g. It will stop decoding a message and start sending it to serial at
- *      precisely the time when the next message is likely to be transmitted,
- *      and may miss it.
-
- * Set the smallest sized "UNKNOWN" message packets we actually care about.
- * This value helps reduce the false-positive detection rate of IR background
- * noise as real messages. The chances of background IR noise getting detected
- * as a message increases with the length of the kTimeout value. (See above)
- * The downside of setting this message too large is you can miss some valid
- * short messages for protocols that this library doesn't yet decode.
- *
- * Set higher if you get lots of random short UNKNOWN messages when nothing
- * should be sending a message.
- * Set lower if you are sure your setup is working, but it doesn't see messages
- * from your device. (e.g. Other IR remotes work.)
- * NOTE: Set this value very high to effectively turn off UNKNOWN detection.
- */
 const uint16_t kMinUnknownSize = 12;
+const uint8_t kTolerancePercentage = kTolerance;
 
-/* How much percentage lee way do we give to incoming signals in order to match
- * it?
- * e.g. +/- 25% (default) to an expected value of 500 would mean matching a
- *      value between 375 & 625 inclusive.
- * Note: Default is 25(%). Going to a value >= 50(%) will cause some protocols
- *       to no longer match correctly. In normal situations you probably do not
- *       need to adjust this value. Typically that's when the library detects
- *       your remote's message some of the time, but not all of the time.
- */
-const uint8_t kTolerancePercentage =
-    kTolerance; /* kTolerance is normally 25% */
-
-/* Legacy (No longer supported!)
- *
- * Change to `true` if you miss/need the old "Raw Timing[]" display.
- */
-#define LEGACY_TIMING_INFO false
-/* ==================== end of TUNEABLE PARAMETERS ==================== */
-
-/* Use turn on the save buffer feature for more complete capture coverage. */
 IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
 IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
 decode_results results; /* Somewhere to store the results */
@@ -148,6 +88,42 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin();
+
+  Serial.println("OTA Ready");
 
   Serial.print("connecting to ");
   Serial.print(host);
@@ -250,4 +226,6 @@ void loop() {
       yield();
     }
   }
+
+  ArduinoOTA.handle();
 }
